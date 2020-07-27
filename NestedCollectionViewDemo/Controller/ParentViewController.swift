ec4502046fc387e1b02e05f309fdea17a104b43e
Vishal <vishal.n@intelegain.com>
2020-07-27 20:04:45 +0530

    Initial commit

---
+++ NestedCollectionViewDemo/Controller/ParentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParentViewController.swift
+//  NestedCollectionViewDemo
+//
+//  Created by Vinayak Bhor on 27/07/20.
+//  Copyright © 2020 Vinayak Bhor. All rights reserved.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+    
+    //MARK: Declare variable
+    
+    //MARK: Decalare outlet
+    
+    //MARK: Decalare life cycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Collection View datasource methods
+    
+    //MARK: Collection View delegate methods
+    
+    
+}
